f13df37f573f8ee882f0b2ccb57c396e17735583
Inglar Gaur <inglarg@gmail.com>
2021-07-03 20:30:30 +0300

    Закончен основной функционал. Добавлены комментарии.

--- botHookHandler.js
+++ botHookHandler.js
@@ -1,26 +1,33 @@
-// Объявление констант
-const SpreadsheetId = '17S6qp8a_9gXEyrfF--aVtIucgrb4ecojtPsZ6NG_AQE';
-const ApiBotToken = '1849336221:AAGBmoyYig9kyg30c1T1k0zbrNcayIdwrcA';
+/**
+ * Объявление констант
+ * @type {string}
+ */
+const SpreadsheetId = '17S6qp8a_9gXEyrfF--aVtIucgrb4ecojtPsZ6NG_AQE';       // Id таблицы google sheet для хранения данных
+const ApiBotToken = '1849336221:AAGBmoyYig9kyg30c1T1k0zbrNcayIdwrcA';       // Id бота
 
+/**
+ * Функция обработки запрса от бота
+ * @param e
+ */
 function doPost(e) {
 
     // Получаем сигнал бота
     let update = JSON.parse(e.postData.contents);
 
     // Проверяем, что это сообщение и оно не пустое
-    if (update.hasOwnProperty('message') && update.message.hasOwnProperty('text')) {
+    if (
+        (update.hasOwnProperty('message') && update.message.hasOwnProperty('text')) ||              // Пришло сообщение написанное пользователем
+        (update.hasOwnProperty('callback_query') && update.callback_query.hasOwnProperty('data'))   // Пользователем нажата inline кнопка
+    ) {
 
         // Получаем объект таблицы по id
         let spreadsheet = SpreadsheetApp.openById(SpreadsheetId);
 
         // Объект данных пользователя
-        let userData = {
-            firstName: update.message.from.first_name,
-            lastName: update.message.from.last_name,
-            chatId: update.message.chat.id
-        };
+        let userData = update.message ? update.message.from : update.callback_query.from;
         let currentUser = ResBotUser(spreadsheet, userData);
 
+        // Объект с настройками и сообщениями из соответствующего листа таблицы
         let messagesAndSettings = ResBotSettingsAndMessages(spreadsheet);
 
         // Авторизован ли пользователь?
@@ -30,112 +37,92 @@ function doPost(e) {
             currentUser.sendMessage(messagesAndSettings.getBySlug("/authorizationOffer"));
 
             // Уведомляем ответственного о попытке
-            let authAdmin = ResBotUser(spreadsheet, {chatId: messagesAndSettings.getBySlug("auth_admin_chat_id")});
-            authAdmin.sendMessage(messagesAndSettings.getBySlug("/new_user") + currentUser.getStringUserData());
+            let authAdmin = ResBotUser(spreadsheet, {id: messagesAndSettings.getBySlug("auth_admin_chat_id")});
+            authAdmin.sendMessage(messagesAndSettings.getBySlug("/newUser") + currentUser.getStringUserData());
 
         } else {
 
             // проверяем команду от пользователя
-            let commandText = update.message.text;
+            let commandText = update.callback_query ? update.callback_query.data : "";
 
             switch (commandText) {
                 // Если резолюции
-                case "/resolutions":
-                    // получаем регион пользователя
-                    let regions = ResBotRegions(spreadsheet);
-                    let userRegion = regions.getRegionByUser(userData.chatId);
+                case "resolutions":
 
-                    if(userRegion){
+                    // Получение региона, к которому относится пользователь
+                    let regions = ResBotRegions(spreadsheet);
+                    let userRegion = regions.getRegionByUser(currentUser.getUserId());
+
+                    if (userRegion) {
 
                         // получаем резолюции по региону и отправляем их пользователю
-                        let resolutions = ResBotResolutions(spreadsheet);
+                        let resolutionsObj = ResBotResolutions(spreadsheet);
+                        let resolutionsText = resolutionsObj.getResolutionByRegion(userRegion);
 
-                        if(resolutions){
-                            currentUser.sendMessage(resolutions);
+                        if (resolutionsText) {
+                            currentUser.sendMessage(userRegion + "\n" + resolutionsText);
+                        } else {
+                            currentUser.sendMessage(userRegion + "\n" + messagesAndSettings.getBySlug("/noResolutions"));
                         }
                     }
 
-                break;
+                    break;
 
-                    // Если контакты возвращаем контакты
-                case "/contacts":
+                // Если контакты возвращаем контакты
+                case "contacts":
 
                     currentUser.sendMessage(messagesAndSettings.getBySlug("/contacts"));
+                    break;
+
+                // При прочих случаях отправляем первично сообщение пользователю с кнопками возможгых действий
+                default:
+
+                    currentUser.sendMessage(messagesAndSettings.getBySlug(
+                        "/defaultCommands"),
+                        {"inline_keyboard": [
+                                [{ "text": "Контакты", 'callback_data': 'contacts' }],
+                                [{ "text": "Резолюции", 'callback_data': 'resolutions' }]
+                            ]}
+                    );
+
+            }
+        }
+    }
+}
+
+/**
+ * Поиск элемента в переданном диапазоне яцеек из google таблицы
+ * @param {Object} googleRange      - Объект с диапазоном ячеек google таблицы (двумерный)
+ * @param {String} searchedElem     - Значение с которым сравиваем перебираемые элементы
+ * @param {Number} keyColumnNumb    - Номер столбца со значением для сравнения с переданным
+ * @param {Number} valueColumnNumb  - Номер столбца со значением, которое необходимо вернуть
+ * @returns {null|*}
+ */
+function searchElemInRange(googleRange, searchedElem, keyColumnNumb, valueColumnNumb){
+
+    // Перебираемый элемент
+    let currentElement;
+
+    if(
+        googleRange &&
+        searchedElem &&
+        googleRange.length > 0
+    ){
+
+        keyColumnNumb = +keyColumnNumb;
+        valueColumnNumb = +valueColumnNumb;
+
+        for(let i = 0; i < googleRange.length; i++){
+
+            currentElement = String(googleRange[i][keyColumnNumb]);
+
+            if(currentElement && ~currentElement.indexOf(searchedElem)){
+                return googleRange[i][valueColumnNumb];
             }
         }
     }
 
-
-
-    // получаем сигнал от бота
-    // let update = JSON.parse(e.postData.contents);
-    // let sheet =  SpreadsheetApp.openById('17S6qp8a_9gXEyrfF--aVtIucgrb4ecojtPsZ6NG_AQE').getSheets()[0]
-    let arraysComands = sheet.getRange(2,5, sheet.getLastRow()-1).getValues();
-    let arrayComands = arraysComands.map(function (row){return row[0]})
-
-    // проверяем тип полученного, нам нужен только тип "сообщение"
-    if (update.hasOwnProperty('message')) {
-        let msg = update.message;
-        let chatId = msg.chat.id;
-        let comandNumRow = arrayComands.indexOf(msg.text) + 2;
-
-        // проверяем, является ли сообщение командой к боту
-        //if (msg.hasOwnProperty('entities') && msg.entities[0].type == 'bot_command') {
-
-        // проверяем на название команды - /lastpost
-        //if (msg.text == '/lastpost') {
-
-        // если все проверки пройдены - запускаем код, который ниже,
-        // открываем оглавление нашего канала
-
-
-        // достает последний пст
-        let lastpost = sheet.getRange(comandNumRow, 1, 1,  4).getValues()[0]
-        let message = ' <strong>'+lastpost[0] + '</strong> \n' + lastpost[3]
-
-        //формируем с ним сообщение
-        let payload = {
-            'method': 'sendMessage',
-            //'chat_id': String(chatId),
-            'chat_id': 146037058,
-            'text': "Пользователь: " +  update.message.from.first_name + " " + update.message.from.last_name + ", id чата: " +  update.message.chat.id,
-            'parse_mode': 'HTML'
-        }
-        let data = {
-            "method": "post",
-            "payload": payload
-        }
-
-        // и отправляем его боту (замените API на свой)
-        UrlFetchApp.fetch('https://api.telegram.org/bot' + API_TOKEN + '/', data);
-
-        payload = {
-            'method': 'sendMessage',
-            'chat_id': String(chatId),
-            'text': "Вы не аторизованы, обратитесь к Владимиру Ялунину",
-            'parse_mode': 'HTML'
-        }
-        data = {
-            "method": "post",
-            "payload": payload
-        }
-
-        // и отправляем его боту (замените API на свой)
-        UrlFetchApp.fetch('https://api.telegram.org/bot' + API_TOKEN + '/', data);
-    }
-}
-// }
-//}
-function temp(e) {
-
-    let sheet =  SpreadsheetApp.openById('17S6qp8a_9gXEyrfF--aVtIucgrb4ecojtPsZ6NG_AQE').getSheets()[0]
-    let arraysComands = sheet.getRange(2,5, sheet.getLastRow()-1).getValues();
-    let arrayComands = arraysComands.map(function (row){return row[0]})
-
-    let comandNumRow = arrayComands.indexOf("/Altai_region") + 2;
-
-    Logger.log(API_TOKEN)
-
+    return null;
 }
 
 /**
@@ -147,9 +134,11 @@ function ResBotUser(googleSpreadsheet, userData){
 
     const ResBotAuth = {
 
-        listName:           "Авторизация",
+        sheetName:          "Авторизация",
         googleSpreadsheet:  undefined,
-        userData:           undefined,
+        userId:             undefined,
+        userFirst_name:     undefined,
+        userLast_name:      undefined,
 
         /**
          * Присутствует ли пользователь в таблице
@@ -158,33 +147,35 @@ function ResBotUser(googleSpreadsheet, userData){
         userIsInSheet: function (){
 
             // Объект таблицы существует как и метод получения листа в нём так же передан id чата пользователя
-            if(this.googleSpreadsheet && this.googleSpreadsheet.hasOwnProperty("getSheetByName") && this.userData && this.userData.chatId){
-                let sheet = this.googleSpreadsheet.getSheetByName(this.listName);
-                let users = sheet.getRange(1, 2, sheet.getLastRow()-1).getValues();
+            if(this.googleSpreadsheet && this.googleSpreadsheet.hasOwnProperty("getSheetByName") && this.userId){
+                let sheet = this.googleSpreadsheet.getSheetByName(this.sheetName);
+                let users = sheet.getRange(1, 2, sheet.getLastRow()).getValues();
 
-                if(users.length > 0){
-                    for(let i = 0; i < users.length; i++){
-                        if(users[i][0] && ~users[i][0].indexOf(this.userData.chatId)){
-                            return true;
-                        }
-                    }
-                }
+                return Boolean(searchElemInRange(users, this.userId, 0, 0));
             }
 
             return false;
         },
 
         /**
+         * Получение id пользователя
+         * @returns {String}
+         */
+        getUserId: function (){
+            return this.userId;
+        },
+
+        /**
          * Получение данных пользователя в виде строки
-         * @returns {string}
+         * @returns {String}
          */
         getStringUserData: function (){
 
-            if(this.userData && (this.userData.firstName || this.userData.lastName || this.userData.chatId)){
+            if(this.userFirst_name || this.userLast_name || this.userId){
 
-                return "Пользователь: " + (this.userData.firstName ? this.userData.firstName + " " : "") +
-                    (this.userData.lastName ? this.userData.lastName + " " : "") +
-                    (this.userData.chatId ? this.userData.chatId : "");
+                return "Пользователь: " + (this.userFirst_name ? this.userFirst_name + " " : "") +
+                    (this.userLast_name ? this.userLast_name + "\n" : "") +
+                    "Id пользователя: " + (this.userId ? this.userId : " отсутствует");
 
             }else{
                 return "Данные пользователя отсутствуют";
@@ -193,32 +184,40 @@ function ResBotUser(googleSpreadsheet, userData){
 
         /**
          * Отправка пользователю сообщения
-         * @param {String} message - Текст сообщения
+         * @param {String} message  - Текст сообщения
+         * @param {Object} keyboard - Набор кнопок (опцонально)
          */
-        sendMessage: function (message){
+        sendMessage: function (message, keyboard = undefined){
 
-            if(this.userData && this.userData.chatId) {
+            if(this.userId) {
 
                 //формируем с ним сообщение
                 let payload = {
                     'method': 'sendMessage',
-                    'chat_id': String(this.userData.chatId),
+                    'chat_id': this.userId,
                     'text': String(message),
                     'parse_mode': 'HTML'
                 }
+
+                if(keyboard && keyboard.hasOwnProperty("inline_keyboard")){
+                    payload.reply_markup = JSON.stringify(keyboard);
+                }
+
                 let data = {
                     "method": "post",
                     "payload": payload
                 }
 
-                // и отправляем его боту (замените API на свой)
+                // и отправляем его боту
                 UrlFetchApp.fetch('https://api.telegram.org/bot' + ApiBotToken + '/', data);
             }
         }
     };
 
     ResBotAuth.googleSpreadsheet = googleSpreadsheet;
-    ResBotAuth.userData = userData;
+    ResBotAuth.userId =             String(userData.id);
+    ResBotAuth.userFirst_name =     String(userData.first_name);
+    ResBotAuth.userLast_name =      String(userData.last_name);
 
     return ResBotAuth;
 
@@ -230,20 +229,25 @@ function ResBotUser(googleSpreadsheet, userData){
  */
 function ResBotRegions(googleSpreadsheet){
 
-    let ResBotRegions = {};
+    let ResBotRegions = {
+        sheetName:          "Авторизация",
+        googleSpreadsheet:  undefined,
 
-    ResBotRegions.listName = "Авторизация";
+        /**
+         * Получения региона, соответствующего пользователю
+         * @param {String} userId
+         * @returns {null|*}
+         */
+        getRegionByUser: function (userId){
+
+            let sheet = this.googleSpreadsheet.getSheetByName(this.sheetName);
+            let rows = sheet.getRange(1, 1, sheet.getLastRow(), 2).getValues();
+
+            return searchElemInRange(rows, userId, 1, 0);
+        }
+    };
+
     ResBotRegions.googleSpreadsheet = googleSpreadsheet;
-
-    // Получение массива - регион -> пользователи
-    ResBotRegions.getRegionsAndUsersFromSheet = function (){
-
-    };
-
-    // Получение региона по пользователю
-    ResBotRegions.getRegionByUser = function (){
-
-    };
 
     return ResBotRegions;
 
@@ -255,20 +259,27 @@ function ResBotRegions(googleSpreadsheet){
  */
 function ResBotResolutions(googleSpreadsheet){
 
-    let ResBotResolutions = {};
+    let ResBotResolutions = {
 
-    ResBotResolutions.listName = "Бот";
+        sheetName: "Бот",
+        googleSpreadsheet: undefined,
+
+        /**
+         * Получение резолюций, соответствующих субъекту
+         * @param region
+         * @returns {null|*}
+         */
+        getResolutionByRegion: function (region){
+
+            let sheet = this.googleSpreadsheet.getSheetByName(this.sheetName);
+            let resolutions = sheet.getRange(1, 1, sheet.getLastRow(), 4).getValues();
+
+            return searchElemInRange(resolutions, region, 0, 3);
+
+        }
+    };
+
     ResBotResolutions.googleSpreadsheet = googleSpreadsheet;
-
-    // Получение массива - регион -> резолюции
-    ResBotResolutions.getResolutionsFromSheet = function (){
-
-    };
-
-    // Получение резолюций по региону
-    ResBotResolutions.getResolutionByRegion = function (){
-
-    };
 
     return ResBotResolutions;
 
@@ -282,7 +293,7 @@ function ResBotSettingsAndMessages(googleSpreadsheet){
 
     const ResBotSettingsAndMessages = {
 
-        listName: "Настройки и сообщения",
+        sheetName: "Настройки и сообщения",
         googleSpreadsheet: undefined,
         messages: {},
 
@@ -291,8 +302,8 @@ function ResBotSettingsAndMessages(googleSpreadsheet){
          */
         getFromSheet: function (){
             if(this.googleSpreadsheet && this.googleSpreadsheet.hasOwnProperty("getSheetByName")){
-                let sheet = this.googleSpreadsheet.getSheetByName(this.listName);
-                let rows = sheet.getRange(1, 1, sheet.getLastRow()-1, 2).getValues();
+                let sheet = this.googleSpreadsheet.getSheetByName(this.sheetName);
+                let rows = sheet.getRange(1, 1, sheet.getLastRow(), 2).getValues();
 
                 if(rows.length > 0){
                     for(let i = 0; i < rows.length; i++){
